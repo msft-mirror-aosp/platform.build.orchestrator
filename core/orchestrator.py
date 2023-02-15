294a7d39a21beec8dc1509fd0bd02cdc0a46c064
Spandan Das <spandandas@google.com>
2023-02-15 21:57:34 +0000

    Add TARGET_BUILD_VARIANT env var in ninja execution
    Currently, this is only set during analysis. This is necessary in
    execution for builds of files like
    packages/modules/Virtualization/authfs/service/src/main.rs
    
    Test: multitree_build system/systemimage (without api_surfaces)
    Change-Id: I45a128bd2f66ee5f279ecb110a2051b74b5bc708

--- core/orchestrator.py
+++ core/orchestrator.py
@@ -204,6 +204,7 @@ class Orchestrator():
         # different JDK toolchains.)
         # For now, set `OUT_DIR` which is inner tree agnostic.
         jail_cfg.add_envar(name="OUT_DIR", value=utils.choose_out_dir())
+        jail_cfg.add_envar(name="TARGET_BUILD_VARIANT", value=self.variant)
         # Disable network access in the combined ninja execution
         jail_cfg.add_option(name="clone_newnet", value="true")
         ninja_runner.run_ninja(context, jail_cfg, targets)
